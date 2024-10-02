5ced3fa1c1a7468d096719689c2b4d12e76b7494
niharika <sahuniharika1211@gmail.com>
2024-10-02 18:23:07 +0530

    Fix: changes in main

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' && process.env.DEPLOY_PLATFORM === 'github' 
-    ? '/portfolio-website/'  // GitHub Pages
-    : '/',  // Netlify
+    ? '/portfolio-website/' // Base path for GitHub Pages
+    : '/', // Base path for other environments like Netlify
 });
