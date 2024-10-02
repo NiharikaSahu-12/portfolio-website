08985f54d63ed06d11311fbb7bdd93eee9f33502
niharika <sahuniharika1211@gmail.com>
2024-10-02 17:38:53 +0530

    Fix: changes in main

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/', // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/',  // Use '/' for Netlify to deploy at the root domain
 });
